9428578e5f9b57e33a13545052621f2b38453758
wavesape <wavesape@126.com>
2018-05-23 22:58:36 +0800

    观察者 基本结构

---
+++ src/observer/ConcreteSubject.java
@@ -0,0 +1,23 @@
+package observer;
+
+
+import java.util.Observer;
+
+/**
+ * ConcreteSubject（具体目标）：具体目标是目标类的子类，通常它包含有经常发生改变的数据，
+ * 当它的状态发生改变时，向它的各个观察者发出通知；同时它还实现了在目标类中定义的抽象业务逻辑方法（如果有的话）。
+ * 如果无须扩展目标类，则具体目标类可以省略。
+ */
+public class ConcreteSubject extends Subject {
+    /**
+     * 实现通知方法
+     */
+    @Override
+    void notice() {
+        observers.forEach((Observer x) ->{
+            // x.response();
+        });
+    }
+
+
+}
---
+++ src/observer/Subject.java
@@ -0,0 +1,30 @@
+package observer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observer;
+
+/**
+ * 定义一个抽象目标类 Subject  （可以称他为宏观目标类）
+ * Subject（目标）：目标又称为主题，它是指被观察的对象。在目标中定义了一个观察者集合，
+ * 一个观察目标可以接受任意数量的观察者来观察，它提供一系列方法来增加和删除观察者对象，
+ * 同时它定义了通知方法notify()。目标类可以是接口，也可以是抽象类或具体类。
+ */
+abstract class Subject {
+    //定义一个观察者集合用来存储所有观察者的对象
+    protected List<Observer> observers =  new ArrayList<>();
+
+    //往观察者集合中添加一个观察者
+    public void addObserver(Observer observer){
+        boolean add = observers.add(observer);
+    }
+
+    //删除一个观察者
+    public void delete(Observer observer){
+        observers.remove(observer);
+    }
+
+    //声明一个抽象通知方法
+    abstract void notice();
+
+}
